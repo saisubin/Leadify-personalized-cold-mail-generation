250841f273df85d412070e15f41fb42cefccd9ba
Sai Subin S V <subins@chimeratechnologies.com>
2026-01-13 00:36:51 +0530

    Remove test and utility scripts

--- server/write-env.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const content = `AZURE_CLIENT_ID=YOUR_CLIENT_ID
-AZURE_CLIENT_SECRET=YOUR_CLIENT_SECRET
-AZURE_TENANT_ID=common
-`;
-fs.writeFileSync('.env', content, 'utf8');
-console.log('.env file written with placeholder values');
